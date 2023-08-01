b4de8587ebb41e43c91c6bfa3cb5785d325fefc4
施杰 <1807872045@qq.com>
2023-08-01 09:04:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/member/skills.html',
+        controller: 'skillsController',
+        controllerAs: 'skillsCtrl'
+    };
+}
+
